3475335cb9c0fa1c704ddc0f1ad6bd930794a7d2
rhgsx <rhgsx@rhgsx.space>
2025-08-05 00:53:57 +0300

    [Minor[ Update supported models

--- bot/handlers/gemini/settings.py
+++ bot/handlers/gemini/settings.py
@@ -13,7 +13,7 @@ GEMINI_ARGS = {
     "search": {"type": "bool"},
     "delete": {"type": "bool"},
     "skipmsg": {"type": "bool"},
-    "model": {"type": "string", "variants": ["gemini-1.5-flash", "gemini-1.5-pro", "gemini-2.0-flash", "gemini-2.5-flash-preview-04-17"]},
+    "model": {"type": "string", "variants": ["gemma-3n-e2b-it", "gemma-3n-e4b-it", "gemma-3-1b-it", "gemma-3-4b-it", "gemma-3-12b-it", "gemma-3-27b-it", "gemini-2.0-flash", "gemini-2.0-flash-lite", "gemini-2.5-pro", "gemini-2.5-flash", "gemini-2.5-flash-lite"]},
     "max_chat_size": {"type": "int"},
     "system_instruction": {"type": "string", "long": True}   
 }
